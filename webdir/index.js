c0ae804f33fff3c08a1842939dfbf73c95b07bd0
ryanmccarth <ryanfmccarthy30@gmail.com>
2019-12-06 16:33:43 -0500

    Moved loading of top moving to js
    Removed hard coded html for carousel, now is hard coded in front end js

--- webdir/index.js
+++ webdir/index.js
@@ -1,19 +1,18 @@
 function showTopMovies(){
-  const $firstitem=$("#firstItem");
-  const $seconditem=$("#secondItem");
-  const $thirditem=$("#thirdItem");
-  const $fourthitem=$("#fourthItem");
+  const $carousel=$("figure");
 
-  $firstitem.empty();
-  $seconditem.empty();
-  $thirditem.empty();
-  $fourthitem.empty();
+  $carousel.empty();
+  let topMovies = new Array();
 
-  $.get("/server", (movies) => {
-     movies.forEach(function(movie) {
+  // $.get("/server", (movies) => {
+  //    movies.forEach(function(movie) {
+  //     topMovies.push(movie);
+  //    });
+  // });
 
-     });
-  });
+  $carousel.append(
+    '<img src="images/sm1.jpg" id = "cposter1" alt=""><img src="images/sm1.jpg" id = "cposter2" alt=""><img src="images/sm1.jpg" id = "cposter3" alt=""><img src="images/sm2.jpg" id ="cposter4"  alt=""><img src="images/sm3.jpg" id = "cposter5" alt="">	<img src="images/sb.jpg" id = "cposter6" alt=""><img src="images/gg.jpg"  id = "cposter7" alt=""><img src="images/joker.jpg" id="cposter8" alt="">'
+  );
 }
 
 function getReccomendations(){
